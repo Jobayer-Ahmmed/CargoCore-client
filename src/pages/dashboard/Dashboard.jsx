00a988ab057db227d4af3527bae26acc9cb6cfe5
Jobayer Ahmmed <jobayer.ahmmed.jas@gmail.com>
2024-10-29 11:44:22 +0600

    add: default settings

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -2,6 +2,6 @@
 
 export default function Dashboard() {
   return (
-    <div><h1>Bangladesh</h1></div>
+    <div className="border-2 border-red-400 pl-p_xl"><h1>Bangladesh</h1></div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors:{
+      textColor:"#3A3C3F",
+    },
+    extend: {
+      spacing:{
+        pXL:"80px",
+      }
+    },
   },
   plugins: [],
 }
